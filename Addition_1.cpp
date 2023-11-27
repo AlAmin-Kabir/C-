1df378cb7df1e3e3e0522669fc74d1b72b94eb1e
Al-Amin Kabir <152113803+AlAmin-Kabir@users.noreply.github.com>
2023-11-27 07:52:08 +0600

    Update Addition_1.cpp

--- Addition_1.cpp
+++ Addition_1.cpp
@@ -1,16 +1,20 @@
 #include<iostream>
 using namespace std;
 int main(void)
-{
+{    
+    //Declaring variables
     float num1,num2,i;
     double sum=0;
+    //Taking number of inputs
     cout<<"How many numbers do you want to add?\n "<<'='<<' ';
     cin>>num1;
     for(i=1;i<=num1;i++)
     {
         cout<<"Input No "<<i<<" : "; 
        cin>>num2;
+    //Addition
        sum=sum+num2;
     }
+    //Printing the sum
     cout<<"The sum is = "<<sum;
 }
